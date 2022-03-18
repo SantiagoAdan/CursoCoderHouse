fe38a532cbefb1483c4704c2f564015c785a5f04
SantiagoAdan <santiadan25@gmail.com>
2022-03-17 23:36:10 -0300

    guardado para clase 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import "./App.css";
+import ItemListContainer from "./components/ItemListContainer";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <NavBar />
+      <ItemListContainer greeting="Hola Usuario!" />
+    </>
   );
 };
 
