3c2618c6509ab0a0e82acf86036073cd0831f066
SantiagoAdan <santiadan25@gmail.com>
2022-04-10 12:46:43 -0300

    push clase4 arreglado

--- Tareas/src/components/ItemDetail.js
+++ Tareas/src/components/ItemDetail.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import Item from './Item'
 
 const ItemDetail = (product) => {
   return (
@@ -8,7 +7,7 @@ const ItemDetail = (product) => {
 <Container>
   <Row>
     <Col><img src={product.img} alt="" /></Col>
-    <Col>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias quae dolore necessitatibus laborum autem quibusdam laboriosam quisquam sequi illo nihil cum quaerat sunt nostrum eveniet molestiae quod, et, error excepturi pariatur ullam facere quia atque a voluptatibus! Magni, velit, obcaecati unde sint laboriosam impedit sed in magnam consectetur tempore architecto?</Col>
+    <Col>{product.description}</Col>
   </Row>
 </Container>
 
