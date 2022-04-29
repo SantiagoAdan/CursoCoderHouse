acd2d5f1e94702e73f65435ddf0f1acee614da03
SantiagoAdan <santiadan25@gmail.com>
2022-04-28 21:14:14 -0300

    firebase corregido

--- Tareas/src/components/NavBar.js
+++ Tareas/src/components/NavBar.js
@@ -14,13 +14,13 @@ const NavBar = () => {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="Categorias" id="collasible-nav-dropdown">
-            <NavLink to='/category/Procesador' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Procesadores</NavLink>
+            <NavLink to='/category/Procesadores' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Procesadores</NavLink>
               <NavDropdown.Item href="#action/3.2">
-              <NavLink to='/category/PlacasDeVideo' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Placas de Video</NavLink>
+              <NavLink to='/category/Placa de Video' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Placas de Video</NavLink>
                
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
-              <NavLink to='/category/RAM' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Memoria Ram</NavLink>
+              <NavLink to='/category/Memoria Ram' className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Memoria Ram</NavLink>
 
                 </NavDropdown.Item>
               <NavDropdown.Divider />
